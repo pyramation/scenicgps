5b60d802568d7da02914bffca3e0c4db28a4c8cd
scenicgps <scenicgps@ps42498.dreamhostps.com>
2011-04-13 13:28:58 -0700

    fixed passenger file

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,8 @@
 import sys, os
-sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = "scenicgps.settings"
 import django.core.handlers.wsgi
+cwd = os.getcwd()
+myapp_directory = cwd + '/scenicgps'
+sys.path.insert(0,myapp_directory)
+sys.path.append(cwd)
+os.environ['DJANGO_SETTINGS_MODULE'] = "scenicgps.settings"
 application = django.core.handlers.wsgi.WSGIHandler()
