8a16462ad59707e3c63ec7fa8e8f65c81ba7232a
Jack Reilly <jackdreilly@gmail.com>
2011-04-13 11:56:31 -0700

    constants stuff and models

--- scenicgps/scenic/models.py
+++ scenicgps/scenic/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class GeoPt(models.Model):
+    lat = models.FloatField()
+    lng = models.FloatField()
+
+class Route(models.Model):
+    plString = models.CharField(max_length=1000)
+    rating = models.IntegerField()
+
+
+
--- scenicgps/settings.py
+++ scenicgps/settings.py
@@ -1,4 +1,5 @@
 # Django settings for scenicgps project.
+from constants import DATABASES
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,16 +10,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'scenicgps_db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
