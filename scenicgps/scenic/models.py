369e851bd441e6fb4e327705eb71dc34d1e51841
scenicgps <scenicgps@ps42498.dreamhostps.com>
2011-04-17 11:44:08 -0700

    normalized database models and unique ratings

--- scenicgps/scenic/models.py
+++ scenicgps/scenic/models.py
@@ -7,7 +7,13 @@ class GeoPt(models.Model):
 
 class Route(models.Model):
     plString = models.CharField(max_length=1000)
+
+class Device(models.Model):
+    device = models.CharField(max_length=40, unique=True)
+
+class Rating(models.Model):
+    device = models.ForeignKey(Device)
+    route  = models.ForeignKey(Route, related_name="%(app_label)s_%(class)s_related")
     rating = models.IntegerField()
-
-
-
+    class Meta:
+        unique_together = ("device", "route")
--- scenicgps/scenic/views.py
+++ scenicgps/scenic/views.py
@@ -4,15 +4,35 @@ from django.http import HttpResponse
 from scenicgps.scenic.models import *
 from django.utils.simplejson import dumps
 
-
 def index(request):
     return HttpResponse('hello world')
 
 def putRoute(request):
     plString = request.GET['plstring']
     rating = int(request.GET['rating'])
+    device = request.GET['device']
     try:
-        Route(plString = plString, rating = rating).save()
+
+	try:
+            rte = Route.objects.get(plString = plString)
+	except:
+            rte = Route(plString = plString)
+	    rte.save()
+
+	try:
+            dev=Device.objects.get(device=device)
+        except:
+            dev=Device(device=device)
+            dev.save()
+
+        try:
+	    rate = Rating.objects.get(route = rte, device = dev)
+            rate.rating = rating
+            rate.save()
+        except:
+            Rating(route = rte, device = dev, rating = rating).save()
+
+
         return HttpResponse(dumps({'status':'ok'}))
     except:
         return HttpResponse(dumps({'status':'error'}))
